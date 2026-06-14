f35ce3aec106c864b48933c812ad9da977b14d8b
idkyeshehe321 <mikeamongus12345@gmail.com>
2026-06-14 10:27:28 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,5 +1,4 @@
 local module = {
-    ActiveBugs = {},
     ActiveHooks = {},
     CachedRemotes = {}
 }
@@ -20,69 +19,38 @@ end
 
 local function startGlobalHook()
     if originalNamecall then return end
-
+    
     originalNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
         local args = {...}
         local method = getnamecallmethod()
         local isRemoteCall = (method == "FireServer" or method == "InvokeServer")
-
+        
         if isRemoteCall then
             local remoteName = tostring(self.Name)
-
-            if module.ActiveBugs[remoteName] then
-                module.ActiveBugs[remoteName].Args = args
-            end
-
+            
             if module.ActiveHooks[remoteName] then
+                
                 local customReturn = module.ActiveHooks[remoteName](self, args)
+                
                 if type(customReturn) == "table" then
                     args = customReturn
                 end
             end
         end
-
+        
         return originalNamecall(self, unpack(args))
     end))
-end
-
-function module.BugRemote(remoteName)
-    if not findRemote(remoteName) then
-        return nil
-    end
-
-    if module.ActiveBugs[remoteName] then 
-        return module.ActiveBugs[remoteName] 
-    end 
-    
-    startGlobalHook()
-    
-    local tracker = {
-        Name = remoteName,
-        Args = {}
-    }
-    
-    module.ActiveBugs[remoteName] = tracker
-    return tracker
-end
-
-function module.DebugRemote(remoteName)
-    if module.ActiveBugs[remoteName] then
-        module.ActiveBugs[remoteName] = nil
-        return "success"
-    else
-        return "no active bug found"
-    end
 end
 
 function module.HookRemote(remoteName, callback)
     if not findRemote(remoteName) then
         return "remote not found"
     end
-
+    
     if type(callback) ~= "function" then
         return "invalid callback"
     end
-
+    
     startGlobalHook()
     module.ActiveHooks[remoteName] = callback
     return "success"
