a0e1fe4a14eeb812cda4300f77e3e09da01d8c54
idkyeshehe321 <mikeamongus12345@gmail.com>
2026-06-16 10:02:23 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -3,6 +3,7 @@ local module = {}
 local players = game:GetService("Players")
 
 function module.Get(obj)
+   print("-----------------------------------------")
    for _, v in next, obj:GetChildren() do
       if not players:FindFirstChild(v.Name) then
          print(v.Name.. ": ".. v.ClassName)
@@ -10,6 +11,7 @@ function module.Get(obj)
          warn(v.Name.. ": ".. v.ClassName)
       end
    end
+   print("-----------------------------------------")
 end
 
 return module
