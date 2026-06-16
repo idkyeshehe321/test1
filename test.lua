b9b38b966143932b7c200ab9167ab9fd3ff2f53a
idkyeshehe321 <mikeamongus12345@gmail.com>
2026-06-16 08:39:08 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,7 +2,7 @@ local module = {}
 
 function module.Get(obj)
    for _, v in next, obj:GetDescendants() do
-      print(v.. ": ".. v.ClassName)
+      print(v.Name.. ": ".. v.ClassName)
    end
 end
 
