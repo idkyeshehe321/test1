beca8790e379043f1864ce8ae311581348cc1475
idkyeshehe321 <mikeamongus12345@gmail.com>
2026-06-16 08:29:57 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,7 +1,7 @@
 local module = {}
 
 function module.Get(obj)
-   for _, v in next, obj:GetDecendents() do
+   for _, v in next, obj:GetDescendents() do
       print(v.. ": ".. typeof(v))
    end
 end
